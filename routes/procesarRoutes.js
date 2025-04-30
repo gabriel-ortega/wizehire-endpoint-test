dec988dbc6e5a08aac19522862ac5359a3ac9baa
gabriel-ortega <gabriel-ortega16@hotmail.com>
2025-04-30 10:21:48 -0500

    only serverless

--- routes/procesarRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { procesarPost } = require('../controllers/procesarController');
-
-// POST /api/procesar
-router.post('/procesar', procesarPost);
-
-module.exports = router;
