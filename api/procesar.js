9623d0ca88d6fe843440311d0c6740b027828952
gabriel-ortega <gabriel-ortega16@hotmail.com>
2025-04-30 10:25:11 -0500

    test

--- api/procesar.js
+++ api/procesar.js
@@ -1,28 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import serverless from 'serverless-http';
+// import express from 'express';
+// import cors from 'cors';
+// import serverless from 'serverless-http';
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json()); // <--- importante para leer req.body
+// app.use(cors());
+// app.use(express.json()); // <--- importante para leer req.body
 
-const data = {
-  account: {
-    companyId: 'newtestcompanyId123',
-  },
-  contact: {
-    emailVerified: true,
-  },
-};
+// const data = {
+//   account: {
+//     companyId: 'newtestcompanyId123',
+//   },
+//   contact: {
+//     emailVerified: true,
+//   },
+// };
 
-app.post('/procesar', async (req, res) => {
-  console.log('Body recibido:', req.body);
+// app.post('/procesar', async (req, res) => {
+//   console.log('Body recibido:', req.body);
 
-  // Esperar 300 milisegundos (0.3 segundos)
-  await new Promise(resolve => setTimeout(resolve, 300));
+//   // Esperar 300 milisegundos (0.3 segundos)
+//   await new Promise(resolve => setTimeout(resolve, 300));
 
-  res.json(data);
-});
+//   res.json(data);
+// });
 
-export default serverless(app);
+// export default serverless(app);
+
+export function GET(request: Request) {
+  return new Response('Hello from Vercel!');
+}
