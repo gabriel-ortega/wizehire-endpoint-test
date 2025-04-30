c08343234b0da869119d44c20a46833ad0d2a7d0
gabriel-ortega <gabriel-ortega16@hotmail.com>
2025-04-29 23:46:23 -0500

    new test data JSON

--- api/procesar.js
+++ api/procesar.js
@@ -5,10 +5,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const datatest = { message: 'asdkjasdkj12983asdlkjasd' };
+const data = {
+  "account": {
+    "companyId": "newtestcompanyId123"
+  },
+  "contact": {
+    "emailVerified": true
+  }
+};
+
+
 app.post('/api/procesar', async (req, res) => {
   console.log('Recibido body:', req.body);
   await new Promise(resolve => setTimeout(resolve, 10000));
-  res.json({ message: 'asdkjasdkj12983asdlkjasd' });
+  res.json(data);
 });
 
 // Exporta como un handler de serverless
