0c7175e564262aee264524c2ec053d3dd073f4b5
gabriel-ortega <gabriel-ortega16@hotmail.com>
2025-04-30 11:30:44 -0500

    Update procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -1,34 +1,23 @@
-// import express from 'express';
-// import cors from 'cors';
-// import serverless from 'serverless-http';
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// const app = express();
+export async function POST(request) {
+  const body = await request.json();
+  console.log('Body recibido:', body);
 
-// app.use(cors());
-// app.use(express.json()); // <--- importante para leer req.body
+  await delay(300); // espera 300 ms
 
-const data = {
-  account: {
-    companyId: 'newtestcompanyId123',
-  },
-  contact: {
-    emailVerified: true,
-  },
-};
+  const data = {
+    account: {
+      companyId: 'newtestcompanyId123',
+    },
+    contact: {
+      emailVerified: true,
+    },
+  };
 
-// app.post('/procesar', async (req, res) => {
-//   console.log('Body recibido:', req.body);
-
-//   // Esperar 300 milisegundos (0.3 segundos)
-//   await new Promise(resolve => setTimeout(resolve, 300));
-
-//   res.json(data);
-// });
-
-// export default serverless(app);
-
-export function POST(request) {
-  console.log('Body recibido:', request.body);
-  // return new Response(data);
-  return new Response("hello world");
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
